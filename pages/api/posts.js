8875076de6a9ac39ff6d3c16393e9e9107feb421
Di Zhu <dzhu@onekingslane.com>
2022-03-26 15:33:53 -0400

    limit page result to 10

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,6 +1,6 @@
 import { PostModel } from "./db"
 
 export default async function handler(req, res) {
-  const response = await PostModel.find().select('-content')
+  const response = await PostModel.find().select('-content').limit(10)
   res.json(response)
 }
